c1826ee13e590a5aed715bac1b80fb67a663ca94
tintinnkt <t.tintinnkt@gmail.com>
2024-12-15 00:20:14 +0700

    feat : add pritteir and minor change {web title name, remove auto-generated tsx element}

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
-import { Code } from "@nextui-org/react";
+import ProjectCard from "./component/ProjectCard";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="flex justify-between w-full">
-        <a
-          href="https://vite.dev"
-          target="_blank"
-          className="w-fit items-center flex"
-        >
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a
-          href="https://react.dev"
-          target="_blank"
-          className="w-fit flex items-center"
-        >
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Code>pnpm i this project</Code>
-    </>
+    <React.Fragment>
+    <nav ></nav>
+    <main>
+      <h1>Hello Jarrrr</h1>
+      <ul>
+        <li><ProjectCard></ProjectCard></li>
+      </ul>
+    </main>
+    </React.Fragment>
   );
 }
 
