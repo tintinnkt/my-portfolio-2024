c8c29adb757deed6f80f21bcfb7dc2c4e8736f82
tintinnkt <t.tintinnkt@gmail.com>
2024-12-14 23:17:15 +0700

    feat : add  nextUI and tailwind/typography

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Code } from "@nextui-org/react";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,7 +21,6 @@ function App() {
           href="https://react.dev"
           target="_blank"
           className="w-fit flex items-center"
-          
         >
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
@@ -37,6 +37,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Code>pnpm i this project</Code>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+import { nextui } from "@nextui-org/react";
+import typography from "@tailwindcss/typography";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui(), typography()],
+};
+
+export default config;
