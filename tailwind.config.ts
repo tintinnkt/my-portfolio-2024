c5d63f6bcd4bcb7b99051407e3b74a4946e3fd7c
tintinnkt <t.tintinnkt@gmail.com>
2024-12-17 15:28:15 +0700

    fix : nextUI not displayed properly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import typography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+
   theme: {
     extend: {},
   },
