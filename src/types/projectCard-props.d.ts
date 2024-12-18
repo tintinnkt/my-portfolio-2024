c62b31aa38bdbfe32efabcbb36831573b34394cc
tintinnkt <t.tintinnkt@gmail.com>
2024-12-18 15:12:17 +0700

    feat: add status (string array) and hightlight (bool)

--- src/types/projectCard-props.d.ts
+++ src/types/projectCard-props.d.ts
@@ -1,13 +1,12 @@
-export interface ProjectCardProps {
-  project: project;
-}
-interface project {
+export interface project {
   title: string;
   subtitile?: string;
   description: string;
   detail?: React.ReactNode;
   tools?: Array<projectTool>;
   type: "project" | "competition";
+  status: Array<string>;
+  highlight: boolean;
 }
 interface projectTool {
   name: string;
