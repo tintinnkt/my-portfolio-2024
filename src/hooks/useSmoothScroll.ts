1de810794721d299002e062dba36851baa232489
tintinnkt <t.tintinnkt@gmail.com>
2024-12-23 22:46:03 +0700

    fix: update smooth scroll hook after top of page have nav

--- src/hooks/useSmoothScroll.ts
+++ src/hooks/useSmoothScroll.ts
@@ -1,16 +1,23 @@
-import { MouseEvent } from "react";
+import { useCallback } from "react";
 
 export const useSmoothScroll = () => {
-  const handleScroll = (e: MouseEvent, id: string) => {
-    e.preventDefault();
-    const target = document.getElementById(id);
-    if (target) {
-      target.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  };
+  const handleScroll = useCallback(
+    (e: React.MouseEvent<HTMLAnchorElement>, targetId: string) => {
+      e.preventDefault();
+      const element = document.getElementById(targetId);
+      const navHeight = 180; // Adjust this value based on your nav height
+
+      if (element) {
+        const topPosition =
+          element.getBoundingClientRect().top + window.scrollY - navHeight;
+        window.scrollTo({
+          top: topPosition,
+          behavior: "smooth",
+        });
+      }
+    },
+    [],
+  );
 
   return { handleScroll };
 };
