ca026fc9f5ab64186402d631759c1e4d81a0db31
tintinnkt <t.tintinnkt@gmail.com>
2025-01-01 16:21:34 +0700

    fix: change smooth scroll hook to support onPress

--- src/hooks/useSmoothScroll.ts
+++ src/hooks/useSmoothScroll.ts
@@ -1,23 +1,19 @@
 import { useCallback } from "react";
 
 export const useSmoothScroll = () => {
-  const handleScroll = useCallback(
-    (e: React.MouseEvent<HTMLAnchorElement>, targetId: string) => {
-      e.preventDefault();
-      const element = document.getElementById(targetId);
-      const navHeight = 65; // Adjust this value based on your nav height
+  const handleScroll = useCallback((targetId: string) => {
+    const element = document.getElementById(targetId);
+    const navHeight = 75; // Adjust this value based on your nav height
 
-      if (element) {
-        const topPosition =
-          element.getBoundingClientRect().top + window.scrollY - navHeight;
-        window.scrollTo({
-          top: topPosition,
-          behavior: "smooth",
-        });
-      }
-    },
-    [],
-  );
+    if (element) {
+      const topPosition =
+        element.getBoundingClientRect().top + window.scrollY - navHeight;
+      window.scrollTo({
+        top: topPosition,
+        behavior: "smooth",
+      });
+    }
+  }, []);
 
   return { handleScroll };
 };
